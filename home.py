7a942a49e3e0b68884d41d325dd218484e84c12c
abhigenie92 <abhigenie92@gmail.com>
2016-03-04 03:17:31 +0530

    add

--- home.py
+++ home.py
@@ -1,11 +1,16 @@
 #https://github.com/udacity/APIs/tree/master/Lesson_3/06_Adding%20Features%20to%20your%20Mashup/Solution%20Code
 #https://www.youtube.com/playlist?list=PLXmMXHVSvS-CoYS177-UvMAQYRfL3fBtX
-from flask import Flask
+from flask import Flask,jsonify, request
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/login',methods=['POST'])
 def home():
-    return  "This is from Flas again!!!"
+	data_received = {'username' : request.json['username'], 'password' : request.json['password']}
+	# Check if password is correct
+
+    return jsonify({'data_received' : data_received})
+
+@app.route('/create',methods=['POST'])
 
 if __name__ == "__main__":
     app.run()
